29b0071fb01b809a841e2f13f86707364575f955
p.korolev <amlerok96@gmail.com>
2022-05-13 23:01:58 +0300

    fix testbase

--- src/test/java/testbase/TestBase.java
+++ src/test/java/testbase/TestBase.java
@@ -6,10 +6,12 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static config.ConfigHelper.getWebRemoteDriverPassword;
 import static config.ConfigHelper.getWebRemoteDriverUser;
 
@@ -17,8 +19,6 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
         baseUrl = "https://www.wildberries.ru/";
         browserSize = "1920x1080";
  //       Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
@@ -30,11 +30,16 @@ public class TestBase {
         Configuration.browserCapabilities = capabilities;
     }
 
+    @BeforeEach
+    void allure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
     @AfterEach
     void addAttachments() {
         Attaches.screenshotAs("Последний скриншот");
         Attaches.pageSource();
         Attaches.browserConsoleLogs();
         Attaches.addVideo();
+        closeWebDriver();
     }
 }
