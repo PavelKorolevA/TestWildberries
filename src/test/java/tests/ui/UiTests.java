03f914060a663c67e2f7fc88e6317f8f9dc41e82
p.korolev <amlerok96@gmail.com>
2022-05-17 22:37:12 +0300

    add step

--- src/test/java/tests/ui/UiTests.java
+++ src/test/java/tests/ui/UiTests.java
@@ -6,6 +6,7 @@ import pages.WebPages;
 import testbase.TestBase;
 
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class UiTests extends TestBase {
 
@@ -17,53 +18,110 @@ public class UiTests extends TestBase {
     @Test
     @DisplayName("Проверка логотипа в хедере")
     void testLogotip() {
+        step("Открыть главную страницу и проверить логотип", () -> {
         webPages
-                .openPage();
+                    .openPage();
+        });
     }
 
     @Test
     @DisplayName("Каталог")
     void testCatalog() {
-        webPages
-                .openPage()
-                .openCatalog()
-                .selectSection()
-                .checkBook();
+        step("Открыть главную страницу", () -> {
+            webPages
+                    .openPage();
+        });
+        step("Открыть каталог", () -> {
+            webPages
+                    .openCatalog();
+        });
+        step("Открыть секцию Книги", () -> {
+            webPages
+                    .selectSection();
+        });
+        step("Проверить, что открыта секция Книги", () -> {
+            webPages
+                    .checkBook();
+        });
     }
 
     @Test
     @DisplayName("Поиск товара")
     void findTovar() {
-        webPages
-                .openPage()
-                .searchField(searchString)
-                .checkRedmond();
+        step("Открыть главную страницу", () -> {
+            webPages
+                    .openPage();
+        });
+        step("Ввести в поиск REDMOND", () -> {
+            webPages
+                    .searchField(searchString);
+        });
+        step("Проверить, что поиск осуществлен по названию REDMOND", () -> {
+            webPages
+                    .checkRedmond();
+        });
     }
 
     @Test
     @DisplayName("Переход на страницу товара")
     void GoToProductPage() {
-        webPages
-                .openPage()
-                .searchField(searchString)
-                .checkRedmond()
-                .productClick()
-                .checkTitleProduct();
+        step("Открыть главную страницу", () -> {
+            webPages
+                    .openPage();
+        });
+        step("Ввести в поиск REDMOND", () -> {
+            webPages
+                    .searchField(searchString);
+        });
+        step("Проверить, что поиск осуществлен по названию REDMOND", () -> {
+            webPages
+                    .checkRedmond();
+        });
+        step("Нажать на товар", () -> {
+            webPages
+                    .productClick();
+        });
+        step("Проверить, что открыта страница выбранного товара", () -> {
+            webPages
+                    .checkTitleProduct();
+        });
     }
 
     @Test
     @DisplayName("Добавление товара в корзину")
     void AddToCart() {
-        webPages
-                .openPage()
-                .searchField(searchString)
-                .checkRedmond()
-                .productClick()
-                .checkTitleProduct()
-                .basketAdd();
+        step("Открыть главную страницу", () -> {
+            webPages
+                    .openPage();
+        });
+        step("Ввести в поиск REDMOND", () -> {
+            webPages
+                    .searchField(searchString);
+        });
+        step("Проверить, что поиск осуществлен по названию REDMOND", () -> {
+            webPages
+                    .checkRedmond();
+        });
+        step("Нажать на товар", () -> {
+            webPages
+                    .productClick();
+        });
+        step("Проверить, что открыта страница выбранного товара", () -> {
+            webPages
+                    .checkTitleProduct();
+        });
+        step("Добавить товар в корзину", () -> {
+            webPages
+                    .basketAdd();
+        });
                  sleep(2000);
-        webPages
-                .goToTheBasket()
-                .checkBasketProduct();
+        step("Перейти в корзину", () -> {
+            webPages
+                    .goToTheBasket();
+        });
+        step("Проверить, что товар добавлен в корзину", () -> {
+            webPages
+                    .checkBasketProduct();
+        });
     }
 }
