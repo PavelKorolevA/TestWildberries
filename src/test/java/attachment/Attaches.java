0a5ae2f94fcfd478a653dfe7ac32f22626e1bd1a
p.korolev <amlerok96@gmail.com>
2022-05-17 09:34:23 +0300

    fix attachment

--- src/test/java/attachment/Attaches.java
+++ src/test/java/attachment/Attaches.java
@@ -1,15 +1,20 @@
 package attachment;
 
 import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+import static com.codeborne.selenide.Selenide.sleep;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
@@ -36,25 +41,33 @@ public class Attaches {
         );
     }
 
-    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-    public static String addVideo() {
-        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-                + getVideoUrl(getSessionId())
-                + "' type='video/mp4'></video></body></html>";
+    public static void addVideo(String sessionId) {
+        URL videoUrl = getVideoUrl(sessionId);
+        InputStream videoInputStream = null;
+        sleep(1000);
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                videoInputStream = videoUrl.openStream();
+                break;
+            } catch (FileNotFoundException e) {
+                sleep(1000);
+            } catch (IOException e) {
+                throw new RuntimeException("[ALLURE VIDEO ATTACHMENT ERROR] Cant attach allure video");
+            }
+        }
+
+        if (videoInputStream != null)
+            Allure.addAttachment("Video", "video/mp4", videoInputStream, "mp4");
     }
 
     private static URL getVideoUrl(String sessionId) {
-        String videoUrl = "https://selenoid.autotests.cloud/videos/" + sessionId + ".mp4";
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId + ".mp4";
 
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
-            e.printStackTrace();
+            throw new RuntimeException("[ALLURE VIDEO ATTACHMENT ERROR] Wrong test video url");
         }
-        return null;
-    }
-
-    private static String getSessionId() {
-        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
     }
 }
